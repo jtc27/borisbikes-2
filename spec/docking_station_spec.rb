fc5001625026bd9f9082a4108d23e5cc92bfbd00
Jacob Cho <jacobcho@live.com>
2021-09-21 17:07:38 +0100

    debug release_bike method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,7 @@
 require 'docking_station'
 
 describe DockingStation do 
+
   it 'expects instances to respond to the release_bike method' do
     expect(DockingStation.new).to respond_to(release_bike) 
   end
